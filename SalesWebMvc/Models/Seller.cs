57bb91b7bbe09b33451e654a9a40d7a5c24f3725
Guilherme Ramos <jg.ramos@live.com>
2022-09-10 10:11:32 -0300

    New migration

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -20,13 +20,14 @@ namespace SalesWebMvc.Models
 
         }
 
-        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary)
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
             Id = id;
             Name = name;
             Email = email;
             BirthDate = birthDate;
             BaseSalary = baseSalary;
+            Department = department;
         }
         public void AddSales(SalesRecord sr)
         {
